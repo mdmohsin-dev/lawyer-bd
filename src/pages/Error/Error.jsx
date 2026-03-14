409d0b554ad74f8ff10a6984d64d4e8845423382
md mohosin <mdmohosin.web@gmail.com>
2026-03-14 12:59:43 +0600

    change text color

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router';
 
 const Error = () => {
     return (
-        <div className='min-h-screen text-white flex flex-col justify-center items-center gap-7'>
+        <div className='min-h-screen flex flex-col justify-center items-center gap-7'>
             <h1 className='text-5xl font-semibold'>Opss !</h1>
             <h3 className='text-2xl'>Page not found</h3>
             <Link className='btn' to='/'>Home</Link>
